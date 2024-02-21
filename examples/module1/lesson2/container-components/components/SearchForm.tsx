de68da4ce34a64da51b09e7dadcfc56b105cd137
Kinga Nowakowska <knowakowska@macbook-pro-kinga.home>
2024-02-21 16:55:39 +0100

    get rid of prop drilling

--- examples/module1/lesson2/container-components/components/SearchForm.tsx
+++ examples/module1/lesson2/container-components/components/SearchForm.tsx
@@ -1,46 +1,8 @@
-import { genderOptions } from '../const/genderOptions';
-import { sortOptions } from '../const/sortOptions';
-import Select from './Select';
-import TextField from './TextField';
+import { PropsWithChildren } from 'react';
 
-type SearchFormProps = {
-  name: string;
-  setName: (name: string) => void;
-  gender: string;
-  setGender: (gender: string) => void;
-  sortOption: string;
-  setSortOption: (sortOption: string) => void;
-};
-
-function SearchForm({
-  name,
-  setName,
-  gender,
-  setGender,
-  sortOption,
-  setSortOption,
-}: SearchFormProps) {
+function SearchForm({ children }: PropsWithChildren) {
   return (
-    <form className="space-x-4 flex items-end justify-center">
-      <TextField
-        label="Name"
-        placeholder="Rick Sanchez..."
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <Select
-        label="Gender"
-        value={gender}
-        onChange={(e) => setGender(e.target.value)}
-        options={genderOptions}
-      />
-      <Select
-        label="Sort by"
-        value={sortOption}
-        onChange={(e) => setSortOption(e.target.value)}
-        options={sortOptions}
-      />
-    </form>
+    <form className="space-x-4 flex items-end justify-center">{children}</form>
   );
 }
 
--- examples/module1/lesson2/container-components/containers/CharacterSearchContainer.tsx
+++ examples/module1/lesson2/container-components/containers/CharacterSearchContainer.tsx
@@ -4,6 +4,10 @@ import SearchForm from '../components/SearchForm';
 import SearchTitle from '../components/SearchTitle';
 import sortCharacters from '../utils/sortCharacters';
 import useCharacters from '../hooks/useCharacters';
+import TextField from '../components/TextField';
+import Select from '../components/Select';
+import { genderOptions } from '../const/genderOptions';
+import { sortOptions } from '../const/sortOptions';
 
 function CharacterSearchContainer() {
   const { name, setName, gender, setGender, characters } = useCharacters();
@@ -16,14 +20,26 @@ function CharacterSearchContainer() {
       <div className="pt-20" />
       <SearchTitle title={'Wyszukiwarka postaci Pokemon'} />
       <div className="pt-8" />
-      <SearchForm
-        name={name}
-        setName={setName}
-        gender={gender}
-        setGender={setGender}
-        sortOption={sortOption}
-        setSortOption={setSortOption}
-      />
+      <SearchForm>
+        <TextField
+          label="Name"
+          placeholder="Rick Sanchez..."
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Select
+          label="Gender"
+          value={gender}
+          onChange={(e) => setGender(e.target.value)}
+          options={genderOptions}
+        />
+        <Select
+          label="Sort by"
+          value={sortOption}
+          onChange={(e) => setSortOption(e.target.value)}
+          options={sortOptions}
+        />
+      </SearchForm>
       <div className="pt-12" />
       <CharacterList characters={sortedCharacters} />
       <div className="pt-16" />
