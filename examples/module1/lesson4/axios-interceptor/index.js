61908677668d1a481b4a82d1c6172994ef9ca9fc
Kinga Nowakowska <knowakowska@macbook-pro-kinga.home>
2024-02-27 11:15:23 +0100

    log request execution time using interceptors

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -1,13 +1,18 @@
 import axios from 'axios';
 
+let startPoint = undefined;
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
+  startPoint = new Date().getTime();
   return config;
 });
 
 // Add a response interceptor
 axios.interceptors.response.use(function (response) {
   // Do something with response data
+  const endPoint = new Date().getTime();
+  const timeDifference = ((endPoint - startPoint) / 1000).toFixed(2);
+  console.log(`The request took about ${timeDifference} seconds.`);
   return response;
 });
 
